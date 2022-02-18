4ea60f08e07d09d9c6dc71f83552bd716217b335
RapVac <89621646+RapVac@users.noreply.github.com>
2022-02-18 18:11:23 -0500

    Add files via upload

---
+++ python_challenge_3.py
@@ -0,0 +1,24 @@
+##Can you get another flag?
+##Maybe change something?
+## Example:
+
+## def hi(text):
+##      print(text)
+##
+## >>> hi("hello")
+## hello
+
+import time
+
+establish = [99, 121, 98, 101, 114, 67, 84, 70, 123, 77, 48, 68, 49, 70, 55, 78, 71, 95, 112, 114, 48, 103, 114, 52, 109, 36, 125]
+
+
+def decrypt(input):
+    return chr(input)
+
+for x in range(0, len(establish)):
+    print(establish[x])
+
+
+print("Just a little bit of writing down...")
+    
