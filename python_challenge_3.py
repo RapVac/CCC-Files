d00b134e9da805f5cd7a67d3536227ad35badabe
RapVac <89621646+RapVac@users.noreply.github.com>
2022-02-22 20:55:31 -0700

    Changed #3

--- python_challenge_3.py
+++ python_challenge_3.py
@@ -2,23 +2,23 @@
 ##Maybe change something?
 ## Example:
 
-## def hi(text):
+## def test(text):
 ##      print(text)
 ##
-## >>> hi("hello")
-## hello
+## >>> test("Robot")
+## Robot
 
 import time
 
 establish = [99, 121, 98, 101, 114, 67, 84, 70, 123, 77, 48, 68, 49, 70, 55, 78, 71, 95, 112, 114, 48, 103, 114, 52, 109, 36, 125]
 
 
-def decrypt(input):
+def decode(input):
     return chr(input)
 
 for x in range(0, len(establish)):
+    establish[x] = establish[x]
     print(establish[x])
 
-
-print("Just a little bit of writing down...")
+print("".join(establish))
     
